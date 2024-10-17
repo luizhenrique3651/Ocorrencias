abe3b2e0f2404e0ad3c5a0872afd41ce72483c87
luiz.coelho <uizhenrique@gmail.com>
2024-10-16 23:06:31 -0400

    Classes Application do Spring boot

---
+++ src/main/java/com/luiz/ocorrencias/OcorrenciasApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.ocorrencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcorrenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcorrenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/ocorrencias/OcorrenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.ocorrencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcorrenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
