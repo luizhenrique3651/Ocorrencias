e5fb142f9648ff223722cc9420ad5c803a62baa5
luiz.coelho <uizhenrique@gmail.com>
2024-10-16 23:08:53 -0400

    Classe de configuração, filtro, Role, Serviço e Gerador de Token, referentes ao uso do Spring Security para autenticação

---
+++ src/main/java/com/luiz/ocorrencias/security/UserRole.java
@@ -0,0 +1,17 @@
+package com.luiz.ocorrencias.security;
+
+public enum UserRole {
+
+	ADMIN("admin"),
+	USER("user");
+	
+	private String role;
+	
+	UserRole(String role){
+		this.role = role;
+	}
+
+	public String getRole() {
+		return role;
+	}
+}
